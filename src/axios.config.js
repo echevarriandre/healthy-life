06c0bd31624b97f1bd4592ca5e3476292d0f1f6a
André Echevarria <echevarriandre@gmail.com>
2023-06-11 16:04:05 +0100

    fix: token not updated after expiring

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,16 +1,39 @@
 import axios from "axios";
 
-let token = null;
-await axios.post(import.meta.env.VITE_PIPEDREAM_URL).then((response) => {
-  token = response.data.token;
-});
-
 const calendlyApi = axios.create({
   baseURL: "https://api.calendly.com/",
-  headers: {
-    Authorization: `Bearer ${token}`,
-    // Authorization: `Bearer ${import.meta.env.VITE_OAUTH_CODE}`,
-  },
 });
+
+calendlyApi.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("calendly_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+calendlyApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      return axios.post(import.meta.env.VITE_PIPEDREAM_URL).then(({ data }) => {
+        localStorage.setItem("calendly_token", data.token);
+        originalRequest.headers["Authorization"] = "Bearer " + data.token;
+        return axios(originalRequest);
+      });
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default calendlyApi;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
+import axios from "axios";
 import { setDefaultOptions } from "date-fns";
 import { pt } from "date-fns/locale";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
+import App from "./App.vue";
 import "./assets/base.css";
+import router from "./router";
 
 setDefaultOptions({ locale: pt });
 
-import App from "./App.vue";
-import router from "./router";
+axios.post(import.meta.env.VITE_PIPEDREAM_URL).then((response) => {
+  localStorage.setItem("calendly_token", response.data.token);
+});
 
 const app = createApp(App);
 
